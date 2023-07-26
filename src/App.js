242cb16a5482529daee19c8a29d3d0314e07ea28
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-07-26 19:31:59 +0700

    declare the class in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 import './App.css';
 
 function App() {
   return <div>
-      <Home /> 
+    <Navbar />
+    <Header />
+    <Home /> 
+    <Footer />
     </div>;
 }
 
