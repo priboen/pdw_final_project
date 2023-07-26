2e09e14bd828e4d8d21e6f23e0d1b71a2faec414
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-07-26 20:05:01 +0700

    create videotron class

---
+++ src/components/Video.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Videotron = () => { 
+    // const videoSrc = Trailer;
+
+    return (
+        <div class="container px-3 px-lg-3 mt-3">
+            <h3>NEW TRAILER CHARACTER GENSHIN IMPACT</h3>
+            <div className="ratio ratio-16x9">
+                <iframe src="https://www.youtube.com/embed/MciOgsEOHZM" title="YouTube video player" allowFullScreen></iframe>
+            </div>
+        </div>
+    );
+}
+
+export default Videotron;
