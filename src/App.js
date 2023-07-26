4126feb41b20eef4279662cd417f07c9b60ad452
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-07-26 20:04:52 +0700

    display videotron class in App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import './App.css';
+import Videotron from './components/Video';
 
 function App() {
   return <div>
     <Navbar />
     <Header />
+    <Videotron />
     <Home /> 
     <Footer />
     </div>;
